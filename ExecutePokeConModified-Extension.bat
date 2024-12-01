4347184fa095631f8b7d7f167cf5a3c974fa41f2
nigirikawa <nigirikawa@gmail.com>
2024-12-02 01:14:12 +0900

    [add] requirements.txtに記載があるライブラリを自動取得する

--- ExecutePokeConModified-Extension.bat
+++ ExecutePokeConModified-Extension.bat
@@ -1,3 +1,7 @@
+python -m pip install --upgrade pip
+
+python -c "import subprocess, sys; subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])"
+
 python SerialController/PokeConUpdateChecker.py
 cd SerialController
 rem python Window.py --profile dragonite
